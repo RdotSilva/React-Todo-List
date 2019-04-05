f3e072dd3934b06868ee6d33496ea2f9af7db4b5
Ryan Silva <ryandotsilva@gmail.com>
2019-04-05 18:28:56 -0400

    Import AddTodo to App.js and add to render method

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Header from "./components/layout/Header";
 import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
 
 class App extends Component {
   state = {
@@ -46,6 +47,7 @@ class App extends Component {
     return (
       <div className='App'>
         <Header />
+        <AddTodo />
         <Todos
           todos={this.state.todos}
           markComplete={this.markComplete}
