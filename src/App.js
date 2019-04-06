b4655d2d782a185cfb100704be3daecee544ec77
Ryan Silva <ryandotsilva@gmail.com>
2019-04-06 11:07:23 -0400

    Add componentDidMount method with axios request

--- src/App.js
+++ src/App.js
@@ -5,11 +5,18 @@ import Todos from "./components/Todos";
 import AddTodo from "./components/AddTodo";
 import About from "./components/pages/About";
 import uuid from "uuid";
+import axios from "axios";
 
 class App extends Component {
 	state = {
 		todos: []
 	};
+
+	componentDidMount() {
+		axios
+			.get("https://jsonplaceholder.typicode.com/todos?_limit=10")
+			.then(res => this.setState({ todos: res.data }));
+	}
 
 	// Toggle Complete
 	markComplete = id => {
@@ -48,7 +55,8 @@ class App extends Component {
 					<div className="container">
 						<Header />
 						<Route
-							exact path="/"
+							exact
+							path="/"
 							render={props => (
 								<React.Fragment>
 									<AddTodo addTodo={this.addTodo} />
